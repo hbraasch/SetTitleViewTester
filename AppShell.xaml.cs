aeb1b2316856d4148abc2e729dfbcd5061b37a32
Heinrich Braasch <hbraasch@users.noreply.github.com>
2022-04-26 08:37:13 +1000

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SetTitleViewTester;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ StartupPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetTitleViewTester
+{
+    internal class StartupPage : ContentPage
+    {
+        public StartupPage()
+        {
+            var titleLabel = new Label { Text = "StartupPage", BackgroundColor = Colors.Orange };
+            NavigationPage.SetTitleView(this, titleLabel);
+
+            Content = new Label { Text = titleLabel.Text };
+        }
+
+    }
+}
